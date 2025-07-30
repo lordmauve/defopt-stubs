e8f6fc2e8ef9ffe0b8aa7bc50911a63e4115e6b0
Daniel Pope <lordmauve@users.noreply.github.com>
2025-07-30 08:39:10 +0100

    Update test script to check generics

--- stubs/defopt-stubs/__init__.pyi
+++ stubs/defopt-stubs/__init__.pyi
@@ -1,50 +1,51 @@
-from typing import Any, Callable, Dict, List, Optional, Union, Literal, Tuple
+from typing import Callable, Dict, List, Optional, Union, Literal, Tuple
 
-Funcs = Union[Callable[..., Any], List[Callable[..., Any]], Dict[str, "Funcs"]]
+type Funcs[T] = Callable[..., T] | List[Callable[..., T]] | Dict[str, Funcs[T]]
+
 
 __version__: str
 
-def run(
-    funcs: Funcs,
+def run[T](
+    funcs: Funcs[T],
     *,
-    parsers: Dict[type, Callable[[str], Any]] = ..., 
+    parsers: Dict[type, Callable[[str], object]] = ..., 
     short: Optional[Dict[str, str]] = ..., 
     cli_options: Literal["kwonly", "all", "has_default"] = ..., 
     show_defaults: bool = ..., 
     show_types: bool = ..., 
     no_negated_flags: bool = ..., 
     version: Union[str, None, bool] = ..., 
-    argparse_kwargs: Dict[str, Any] = ...,
+    argparse_kwargs: Dict[str, object] = ...,
     intermixed: bool = ..., 
     argv: Optional[List[str]] = ...,
-) -> Any: ...
+) -> T: ...
 
-def bind(
-    funcs: Funcs,
+def bind[T](
+    funcs: Funcs[T],
     *,
-    parsers: Dict[type, Callable[[str], Any]] = ...,
+    parsers: Dict[type, Callable[[str], object]] = ...,
     short: Optional[Dict[str, str]] = ..., 
     cli_options: Literal["kwonly", "all", "has_default"] = ..., 
     show_defaults: bool = ..., 
     show_types: bool = ..., 
     no_negated_flags: bool = ..., 
     version: Union[str, None, bool] = ..., 
-    argparse_kwargs: Dict[str, Any] = ...,
+    argparse_kwargs: Dict[str, object] = ...,
     intermixed: bool = ..., 
     argv: Optional[List[str]] = ...,
-) -> Callable[[], Any]: ...
+) -> Callable[[], T]: ...
 
-def bind_known(
-    funcs: Funcs,
+def bind_known[T](
+    funcs: Funcs[T],
     *,
-    parsers: Dict[type, Callable[[str], Any]] = ...,
+    parsers: Dict[type, Callable[[str], object]] = ...,
     short: Optional[Dict[str, str]] = ..., 
     cli_options: Literal["kwonly", "all", "has_default"] = ..., 
     show_defaults: bool = ..., 
     show_types: bool = ..., 
     no_negated_flags: bool = ..., 
     version: Union[str, None, bool] = ..., 
-    argparse_kwargs: Dict[str, Any] = ...,
+    argparse_kwargs: Dict[str, object] = ...,
     intermixed: bool = ..., 
     argv: Optional[List[str]] = ...,
-) -> Tuple[Callable[[], Any], List[str]]: ...
+) -> Tuple[Callable[[], T], List[str]]: ...
