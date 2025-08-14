cd6606122ceae11e58951c47c62b2353b8eb4b14
Daniel Pope <lordmauve@users.noreply.github.com>
2025-08-14 18:18:13 +0100

    Use covariant collections in stubs and add mapping example

--- stubs/defopt-stubs/__init__.pyi
+++ stubs/defopt-stubs/__init__.pyi
@@ -1,8 +1,8 @@
 import types
 import inspect
-from typing import Callable, Dict, List, Optional, Union, Literal, Tuple
+from typing import Callable, Mapping, Sequence, List, Optional, Union, Literal, Tuple
 
-type Funcs[T] = Callable[..., T] | List[Callable[..., T]] | Dict[str, Funcs[T]]
+type Funcs[T] = Callable[..., T] | Sequence[Callable[..., T]] | Mapping[str, Funcs[T]]
 
 
 __version__: str
@@ -10,46 +10,46 @@ __version__: str
 def run[T](
     funcs: Funcs[T],
     *,
-    parsers: Dict[type, Callable[[str], object]] = ..., 
-    short: Optional[Dict[str, str]] = ..., 
+    parsers: Mapping[type, Callable[[str], object]] = ...,
+    short: Optional[Mapping[str, str]] = ...,
     cli_options: Literal["kwonly", "all", "has_default"] = ..., 
     show_defaults: bool = ..., 
     show_types: bool = ..., 
     no_negated_flags: bool = ..., 
     version: Union[str, None, bool] = ..., 
-    argparse_kwargs: Dict[str, object] = ...,
+    argparse_kwargs: Mapping[str, object] = ...,
     intermixed: bool = ..., 
-    argv: Optional[List[str]] = ...,
+    argv: Optional[Sequence[str]] = ...,
 ) -> T: ...
 
 def bind[T](
     funcs: Funcs[T],
     *,
-    parsers: Dict[type, Callable[[str], object]] = ...,
-    short: Optional[Dict[str, str]] = ..., 
+    parsers: Mapping[type, Callable[[str], object]] = ...,
+    short: Optional[Mapping[str, str]] = ...,
     cli_options: Literal["kwonly", "all", "has_default"] = ..., 
     show_defaults: bool = ..., 
     show_types: bool = ..., 
     no_negated_flags: bool = ..., 
     version: Union[str, None, bool] = ..., 
-    argparse_kwargs: Dict[str, object] = ...,
+    argparse_kwargs: Mapping[str, object] = ...,
     intermixed: bool = ..., 
-    argv: Optional[List[str]] = ...,
+    argv: Optional[Sequence[str]] = ...,
 ) -> Callable[[], T]: ...
 
 def bind_known[T](
     funcs: Funcs[T],
     *,
-    parsers: Dict[type, Callable[[str], object]] = ...,
-    short: Optional[Dict[str, str]] = ..., 
+    parsers: Mapping[type, Callable[[str], object]] = ...,
+    short: Optional[Mapping[str, str]] = ...,
     cli_options: Literal["kwonly", "all", "has_default"] = ..., 
     show_defaults: bool = ..., 
     show_types: bool = ..., 
     no_negated_flags: bool = ..., 
     version: Union[str, None, bool] = ..., 
-    argparse_kwargs: Dict[str, object] = ...,
+    argparse_kwargs: Mapping[str, object] = ...,
     intermixed: bool = ..., 
-    argv: Optional[List[str]] = ...,
+    argv: Optional[Sequence[str]] = ...,
 ) -> Tuple[Callable[[], T], List[str]]: ...
 class Parameter(inspect.Parameter):
     doc: Optional[str]
